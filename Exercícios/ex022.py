99eb845585002994f3d8c1c83656d9359c2351f0
Carlos Gabriel <canalcarlosgabriel1407@gmail.com>
2022-02-10 19:08:26 -0300

    Update ex022.py

--- Exercícios/ex022.py
+++ Exercícios/ex022.py
@@ -1,8 +1,8 @@
-nome = input('Qual o seu nome completo?')
+nome = str(input('Qual o seu nome completo?')).strip()
 
 print('''
         {}
         {}
         Seu nome tem {} letras
         Seu 1º nome tem {} letras
-'''.format(nome.upper(), nome.lower(), ))
+'''.format(nome.upper(), nome.lower(), (len(nome)-nome.count(' ')), nome.find(' ')))
